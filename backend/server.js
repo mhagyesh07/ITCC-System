2c1aa9e0aece9f3c6a77d51a4f9271ba0990a551
mhagyesh07 <139583764+mhagyesh07@users.noreply.github.com>
2025-05-17 16:21:39 +0530

    Describe your recent changes briefly

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path');
 // Load environment variables
 dotenv.config();
 
@@ -21,7 +21,12 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use('/api/tickets', require('./routes/ticketRoutes')); // Ticket routes
 app.use('/api/users', require('./routes/userRoutes')); // User routes
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// Handle any unknown routes and serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
